5abd7a08c74b16d9734a33ade36da611188fa7a0
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-06-15 16:08:49 +0300

    modifies

--- app/tests/user_test.py
+++ app/tests/user_test.py
@@ -5,7 +5,7 @@ from app import db
 class UserTest(unittest.TestCase):
     
     def setUp(self):
-        self.new_user = User(username='bihawa',password='qwerty')
+        self.new_user = User(username='abdimulhin.yussuf@student.moringaschool.com',password='moringaschool')
         
     def test_password_setter(self):
         self.assertTrue(self.new_user.password is not None)
@@ -15,4 +15,4 @@ class UserTest(unittest.TestCase):
             self.new_user.password
             
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('qwerty'))
+        self.assertTrue(self.new_user.verify_password('moringaschool'))
